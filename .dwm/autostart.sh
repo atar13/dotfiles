c2d37ee1d25d2f1bbf92c947516ae4412a6a1fa0
Anthony Tarbinian <atar137h@gmail.com>
2021-04-10 21:34:44 -0700

    dwm autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+
+sh /home/atarbinian/.fehbg &
+#feh --bg-scale /usr/share/backgrounds/hao-wang-pVq6YhmDPtk-unsplash.jpg &
+
+#compton &
+picom &
+/home/atarbinian/dev/go/go-dwm-statusbar/go-dwm-statusbar &
+dunst &
