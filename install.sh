ba8f93ce45300f770f130c61600955722fe5fdf4
Anthony Tarbinian <atar137h@gmail.com>
2022-01-30 18:54:07 -0800

    Starter install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#sudo pacman -S zsh neovim starship vim alacritty kitty
+
+mkdir -p ~/.config/alacritty
+ln -s "$(pwd)/alacritty/alacritty.yml" ~/.config/alacritty/alacritty.yml
+
+mkdir -p ~/.config/kitty
+ln -s "$(pwd)/kitty/kitty.conf" ~/.config/kitty/kitty.conf
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim --cmd "PlugInstall" \
+		-c 'qa!'
+mkdir -p ~/.config/nvim
+ln -s "$(pwd)/nvim/init.vim" ~/.config/nvim/init.vim
+ln -s "$(pwd)/nvim/themes" ~/.config/nvim/themes
+
+#sh -c "$(curl -fsSL https://git.io/zinit-install)"
+ln -s "$(pwd)/zsh/.zshrc" ~/.zshrc
