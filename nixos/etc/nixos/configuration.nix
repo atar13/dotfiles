585991a9260d0411f735a170d35b315476215271
Anthony Tarbinian <atar137h@gmail.com>
2023-02-25 15:40:14 -0800

    initial home-manager
    home-manager setup as a nix module in configuration.nix. for now it just symlinks my kitty configuration
    
    also added some more packages (needs to be organized)
    
    added xfce and lightdm

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -2,22 +2,30 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  user="atarbinian";
+in
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [
+      # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      <home-manager/nixos>
     ];
 
   # Nix
   nix.settings.experimental-features = [ "flakes" "nix-command" ];
+
+  #nix-store -q --references /run/current-system/sw | fzf TODO: add this to .zshrc later
 
   # Kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
@@ -49,12 +57,37 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  #services.xserver.windowManager.dwm.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        lightdm.enable = true;
+        defaultSession = "none+dwm";
+      };
+      desktopManager.xfce.enable = true;
+      windowManager.dwm.enable = true;
+    };
+  };
+
+  nixpkgs.config.packageOverrides = {
+    dwm = pkgs.callPackage /home/atarbinian/Pkgs/dwm { };
+    dmenu = pkgs.callPackage /home/atarbinian/Pkgs/dmenu { };
+    go-dwm-statusbar = pkgs.callPackage /home/atarbinian/Pkgs/go-dwm-statusbar { };
+  };
+
+  services.xserver.displayManager.session = [
+    {
+      manage = "window";
+      name = "dwm";
+      start = ''
+        ${pkgs.dwm}/bin/dwm &
+        waitPID=$!
+      '';
+    }
+  ];
+
 
   # Configure keymap in X11
   services.xserver = {
@@ -83,20 +116,106 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.atarbinian = {
+  users.users.${user} = {
     isNormalUser = true;
     description = "Anthony Tarbinian";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "video" ];
     packages = with pkgs; [
       stow
+
       firefox
+      brave
+      bitwarden
+      obsidian
+      discord
+      mupdf
+      pcmanfm
+      CuboCore.corefm
+      ulauncher
+
+      cmus
+      spotify
+      spicetify-cli
+
+      luajit
       neovim
-    #  thunderbird
+      neovide
+      vimPlugins.packer-nvim
+
+      fzf
+      ripgrep
+      fd
+      lfs
+      ffmpeg 
+      yt-dlp
+      croc
+      gocryptfs
+
+      gotop
+      btop
+
+      hollywood
+      neofetch
+      cmatrix
+      onefetch
+
+      tmux
+      kitty
+      alacritty
+      wezterm
+
+      dmenu
+      dwmbar
+
+
+      lm_sensors
+
+      picom-next
+      feh
+      dunst
+      networkmanagerapplet
+      nerdfonts
+      #  thunderbird
+
+      rnix-lsp
+
+      cargo
+      gcc
     ];
   };
+
+  programs.light.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    configure = {
+      packages."packer.nvim" = with pkgs.vimPlugins; {
+        start = [ "packer.nvim" ];
+      };
+    };
+  };
+
+
+  #home-manager.users.atarbinian = {pkgs, ... }: {
+  #	home.username = "atarbinian";
+  #      home.homeDirectory = "/home/atarbinian";
+  #	home.stateVersion = "23.05";
+  #	home.packages = [
+  #      	pkgs.neovim
+  #      ];
+  # 	programs.home-manager.enable = true;
+  #      programs.neovim = {
+  #            enable = true;
+  #            defaultEditor = true;
+  #            viAlias = true;
+  #            extraLuaConfig = lib.fileContents "/home/atarbinian/Pkgs/dotfiles/nvim/.config/nvim/init.lua";
+  #      };
+  #};
+
+
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -104,14 +223,51 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     git
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     refind
-     tealdeer
-     wget
-     gnupg
-     gnumake
+    git
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    efibootmgr
+    refind
+    tealdeer
+    wget
+    gnupg
+    gnumake
+    pulseaudio
+
+    imlib2
+
+    vscodium
+    vscodium.fhs 
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        vscodevim.vim
+        bbenoist.nix
+        ms-python.python
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-ssh-edit";
+          publisher = "ms-vscode-remote";
+          version = "0.47.2";
+          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        }
+      ];
+    })
+
+    lxappearance
+    materia-theme
+    bibata-cursors
+    tela-icon-theme
   ];
+
+
+
+  #nixpkgs.overlays = [
+  # (final: prev: {
+  #   dwm = prev.dwm.overrideAttrs (old: { src = /home/atarbinian/Pkgs/dwm ;});
+  # })
+  #];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -124,7 +280,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -140,4 +296,14 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
+
+  home-manager.users.${user} = { pkgs, ... }: {
+    home.stateVersion = "23.05";
+    home.packages = with pkgs; [ htop ];
+
+    home.file = {
+      "kitty".source = "/home/atarbinian/Pkgs/dotfiles/kitty/.config/kitty/kitty.conf";
+      "kitty".target = ".config/kitty/kitty.conf";
+    };
+  };
 }
