708f91820b5ce39c189bd7f12731fa89bbcd5e24
Anthony Tarbinian <atar137h@gmail.com>
2024-03-21 19:12:37 -0700

    .

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 print("running telescope")
 return {
     'nvim-telescope/telescope.nvim',
-    dir = require("lazy-nix-helper").get_plugin_path("telescope.nvim")
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dir = require("lazy-nix-helper").get_plugin_path("telescope.nvim"),
+    dependencies = { 'nvim-lua/plenary.nvim' }
 }
