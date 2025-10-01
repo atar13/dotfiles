3e14e1deb9ae4aabb39e9cfc4956768d611042ad
Anthony Tarbinian <atar137h@gmail.com>
2025-10-01 10:44:11 -0700

    [nvim] add navic back to lualine

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -17,6 +17,52 @@ local function get_navic()
     return ""
 end
 
+-- Customized navic.get_location() that combines namespaces into a single string.
+-- Example: `adam::bob::charlie > foo` is transformed into `a::b::charlie > foo`
+local function navic_namespace()
+    local navic = require("nvim-navic")
+    local old_data = navic.get_data()
+    local new_data = {}
+    local cur_ns = nil
+    local ns_comps = {}
+
+    for _, comp in ipairs(old_data) do
+        if comp.type == "Namespace" then
+            cur_ns = comp
+            table.insert(ns_comps, comp.name)
+        else
+            -- On the first non-namespace component $c$, collect
+            -- previous NS components into a single one and
+            -- insert it in front of $c$.
+            if cur_ns ~= nil then
+                -- Concatenate name and insert
+                local num_comps = #ns_comps
+                local comb_name = ""
+                for idx = 1, num_comps do
+                    local ns_name = ns_comps[idx]
+
+                    -- No "::" in front of first component
+                    local join = (idx == 1) and "" or "::"
+
+                    if idx ~= num_comps then
+                        comb_name = comb_name .. join .. ns_name:sub(1, 1)
+                    else
+                        comb_name = comb_name .. join .. ns_name
+                    end
+                end
+
+                cur_ns.name = comb_name
+                table.insert(new_data, cur_ns)
+                cur_ns = nil
+            end
+
+            table.insert(new_data, comp)
+        end
+    end
+
+    return navic.format_data(new_data)
+end
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
@@ -42,8 +88,8 @@ require("lualine").setup({
 		lualine_b = { session, "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
 		-- lualine_x = { "%{%v:lua.require'nvim-navic'.get_location()%}", emojis, "encoding", "fileformat", "filetype" },
-		-- lualine_x = { get_navic, emojis, "encoding", "fileformat", "filetype" },
-		lualine_x = { emojis, "encoding", "fileformat", "filetype" },
+		lualine_x = { navic_namespace, emojis, "encoding", "fileformat", "filetype" },
+		-- lualine_x = { emojis, "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
