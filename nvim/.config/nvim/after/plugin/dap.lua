678ba20fb8bbea9287c24d50660486e4e251460f
Anthony Tarbinian <atar137h@gmail.com>
2025-10-01 10:44:42 -0700

    [nvim] dap

---
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -0,0 +1,38 @@
+local dap = require('dap')
+dap.adapters.codelldb = {
+  type = "executable",
+  command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
+
+  -- On windows you may have to uncomment this:
+  -- detached = false,
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+
+local dapui = require("dapui")
+
+dapui.setup()
+
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
